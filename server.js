047bfca76be1f46397dbf199fd5eeed2df78e5a0
Gary Clark <Gary Clark>
2016-08-28 22:01:20 -0500

    Added sample highcharts folder with graphic, going to try out async npm, brought in Rdata.json

--- server.js
+++ server.js
@@ -10,22 +10,35 @@ app.use('/MarchMadness', express.static(__dirname + '/MarchMadness'));
 ///////////
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-server.listen(port, function(){
+//Need to route to highcharts page, NOT WORKING///
+var router = express.Router();
+router.route('/')
+    .get(function(req, res){
+    res.send("Highcharts/hcharts.html");
+});
+//////////////////
+
+server.listen(port, function () {
     console.log("Running on port " + port);
 });
 
 var io = require("socket.io")(server);
+var async = require("async");
 
-io.on("connection", function(socket){
+io.on("connection", function (socket) {
     console.log("Connected on " + port);
-    socket.on("disconnect", function(){
+    socket.on("disconnect", function () {
         console.log("Disconnected from " + port);
     });
-    socket.on("error", function(error){
+    socket.on("error", function (error) {
         console.log(error);
     });
 });
+
+
+
+
